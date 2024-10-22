bbc7e3c925f705a763712e8bf2d7aa019df49dda
Developer Imran <ik1155486@gmail.com>
2024-10-22 20:30:06 +0500

    .Net Website

---
+++ Eproject In .Net/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+using System.Diagnostics;
+using Eproject_In_.Net.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Eproject_In_.Net.Controllers
+{
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
+
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+		public IActionResult About()
+		{
+			return View();
+		}
+		public IActionResult Contact()
+		{
+			return View();
+		}
+		public IActionResult Registered()
+		{
+			return View();
+		}
+
+		public IActionResult Login()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
+}
